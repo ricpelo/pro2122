f28957f424d968e9734e8e76c0476bfa67660aaf
Ricardo Pérez <ricpelo@gmail.com>
2022-01-14 20:55:17 +0100

    Primera versión del Vampiro

---
+++ vampiro/mapa.py
@@ -0,0 +1,17 @@
+"""
+El mapa del juego y todo lo relacionado con las localidades
+del juego.
+"""
+
+NOMBRE = 'nombre'
+DESCRIPCION = 'descripcion'
+
+VESTIBULO = {
+    NOMBRE: 'Vestibulo',
+    DESCRIPCION: 'Estás en el vestíbulo del castillo. El ambiente es muy húmedo y frío. Estás en un pasillo que se extiende hacia el norte. Al sur queda la puerta de entrada al castillo.'
+}
+
+PASILLO = {
+    NOMBRE: 'Pasillo',
+    DESCRIPCION: 'Te encuentras en medio del pasillo principal de este piso. Al oeste está la cocina y al este la biblioteca. El pasillo sigue hacia el norte.'
+}
