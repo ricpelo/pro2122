9dc102528d4fd32ce194af7f27347108b528f86e
Ricardo Pérez <ricpelo@gmail.com>
2022-03-16 17:15:11 +0100

    Miércoles 16 de marzo

---
+++ java/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * ¡Hola, mundo!
+ */
+public class Principal {
+    public static void main(String[] args) {
+        int a = 5, b;
+
+
+
+        {
+            int c = 8;
+            c += 1;
+            c++;
+            c -= 25; // c = c * 25;
+            System.out.println(c);
+            System.out.println(a);
+        }
+
+        System.out.println(a);
+
+        int x;
+    }
+}
